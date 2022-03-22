6d269934776c6c347081ae4f996673bf6e7cd9d7
santiago lopez cane <santiagolopezcane@gmail.com>
2022-03-22 17:11:12 -0300

    finished second exercise

---
+++ EjercicioII/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EjercicioII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exito;
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un numero");
+            exito = int.TryParse(Console.ReadLine(), out numero);
+
+            while(!exito || numero <= 0)
+            {
+                Console.WriteLine("Error. ¡Reingresar numero!");
+                exito = int.TryParse(Console.ReadLine(), out numero);
+            }
+
+            Console.WriteLine($"el numero {numero} al cuadrado es {numero * numero} y al cubo es {numero * numero * numero}");
+        }
+    }
+}
