d10b799cb920cdb0eb2275de2482ca142472ce55
santiago lopez cane <santiagolopezcane@gmail.com>
2022-03-22 16:34:42 -0300

    initial commit

---
+++ EjercicioI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EjercicioI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroActual, minimo = 0, maximo = 0;
+            float promedio = 0;
+            bool exito;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese {5 - i} numeros (de a uno)");
+                exito = int.TryParse(Console.ReadLine(), out numeroActual);
+
+                while(!exito)
+                {
+                    Console.WriteLine("Error, ingrese un numero valido");
+                    exito = int.TryParse(Console.ReadLine(), out numeroActual);
+                }
+
+                promedio += (float) numeroActual / 5;
+
+                if (i == 0 || numeroActual > maximo)
+                {
+                    maximo = numeroActual;
+                }
+
+                if (i == 0 || numeroActual < minimo)
+                {
+                    minimo = numeroActual;
+                }
+            }
+
+            Console.WriteLine("El minimo fue {0}, el maximo {1} y el promedio {2}", minimo, maximo, promedio);
+        }
+    }
+}
